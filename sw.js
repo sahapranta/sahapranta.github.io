561e26eb952da849bfcf8c9114278801bf041e6a
Pranta Saha <sahapranta@gmail.com>
2018-04-17 15:12:25 +0600

    1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('serviceworker-cache-polyfill.js');
 
 self.addEventListener('install', function(event) {
 event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('sahapranta').then(function(cache) {
       return cache.addAll([
         '/index.html',
         '/css/bootstrap.min.css',
@@ -35,7 +35,6 @@ event.waitUntil(
         '/img/footer-logo.png',
         '/img/fav-icon.png',
         '/img/Pattern_Light.png',
-        '/img/logo/36x36.png',
         '/img/logo/60x60.png',
         '/img/logo/72x72.png',
         '/img/logo/128x128.png',
@@ -44,10 +43,22 @@ event.waitUntil(
         '/img/logo/144x144.png',
         '/img/logo/216x216.png',
         '/img/logo/360x360.png',
-        '/img/logo/432x432.png',
-        '/img/logo/576x576.png',
-        '/img/logo/720x720.png'
+        'https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE'
       ]);
     })
  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
 });
