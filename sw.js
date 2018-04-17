e4a6482ab94837c4f90f794612f9d72280b93d31
Pranta Saha <sahapranta@gmail.com>
2018-04-17 15:42:11 +0600

    self

--- sw.js
+++ sw.js
@@ -50,15 +50,15 @@ event.waitUntil(
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
-    );
+    // event.respondWith(
+    //     caches.match(event.request)
+    //         .then(function(response) {
+    //                 // Cache hit - return response
+    //                 if (response) {
+    //                     return response;
+    //                 }
+    //                 return fetch(event.request);
+    //             }
+    //         )
+    // );
 });
