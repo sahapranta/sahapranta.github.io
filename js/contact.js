748e1962d716ec8572682970223b02c7163fcb1e
Pranta Saha <sahapranta@gmail.com>
2018-05-23 12:39:13 +0600

    minifying assets

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
     
     (function($) {
-        "use strict";
-
-    
+        "use strict";  
     jQuery.validator.addMethod('answercheck', function (value, element) {
         return this.optional(element) || /^\bcat\b$/.test(value)
     }, "type the correct answer -_-");
