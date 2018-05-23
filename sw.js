560c73b71aa5c0de761a96ddd86033e35ced60b4
Pranta Saha <sahapranta@gmail.com>
2018-05-23 16:20:41 +0600

    fetch event

--- sw.js
+++ sw.js
@@ -31,5 +31,10 @@ event.waitUntil(
 });
 
 self.addEventListener('fetch', function(event) {
-
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
