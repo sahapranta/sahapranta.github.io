4b4adbce194c7326ac0fbd0ce2c5cabba3cc8dee
Pranta Saha <sahapranta@gmail.com>
2018-05-23 15:10:39 +0600

    refactor

--- gruntfile.js
+++ gruntfile.js
@@ -1,30 +1,45 @@
 module.exports=function(grunt){
 	grunt.initConfig({
 		pkg:grunt.file.readJSON('package.json'),
-    uglify:{
-    	options:{
-    		manage:false
-    	},
-    	my_target:{
-    		files:{
-    			'js/theme.min.js':['js/theme.js', 'js/contact.js']
-    		}
-    	}
-    },
-    	// 	cssmin:{
-	// 		my_target:{
-	// 			files: [{
-	// 				expand:true,
-	// 				cwd: 'css/',
-	// 				src:['style.css', '!*.min.css'],
-	// 				dest:'css/',
-	// 				ext:'.min.css'
-	// 			}]
-	// 		}
-	// 	}
+	    uglify:{
+	    	options:{
+	    		manage:false
+	    	},
+	    	my_target:{
+	    		files:{
+	    			'js/theme.min.js':['js/theme.js', 'js/contact.js']
+	    		}
+	    	}
+	    },
+	    cssmin:{
+			my_target:{
+				files: [{
+					expand:true,
+					cwd: 'css/',
+					src:['style-dev.css', '!*.min.css'],
+					dest:'css/',
+					ext:'.min.css'
+				}]
+			}
+		},
+		htmlmin: {
+			options:{
+				removeComments:true,
+				collaspseWhitespace:true,
+				sortAttributes:true,
+				sortClassName:true,
+				minifyCSS:true,
+				minifyJS:true
+			},
+			my_target:{
+				files:{
+					'index.html':'dev/index.html'
+				}
+			}
+		}
+    });
 
-	 });
-
-	// grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 };
