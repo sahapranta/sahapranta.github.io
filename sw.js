7879f7fd623f5c84cab6d4521155c4c178d7907b
Pranta Saha <sahapranta@gmail.com>
2018-05-23 16:17:46 +0600

    sw

--- sw.js
+++ sw.js
@@ -4,7 +4,10 @@ self.addEventListener('install', function(event) {
 event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
+        '/',
         '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
         '/css/bootstrap.min.css',
         '/css/font-awesome.min.css',
         '/css/style.css',
