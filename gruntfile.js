73c112926569eb804c265702e5bad829c3f1c6bf
Pranta Saha <sahapranta@gmail.com>
2018-05-23 13:57:48 +0600

    aha

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,17 @@
 module.exports=function(grunt){
 	grunt.initConfig({
-		pkg:grunt.file.readJSON('package.json')
-
-	// 	cssmin:{
+		pkg:grunt.file.readJSON('package.json'),
+    uglify:{
+    	options:{
+    		manage:false
+    	},
+    	my_target:{
+    		files:{
+    			'js/theme.min.js':['js/theme.js', 'js/contact.js']
+    		}
+    	}
+    },
+    	// 	cssmin:{
 	// 		my_target:{
 	// 			files: [{
 	// 				expand:true,
@@ -17,4 +26,5 @@ module.exports=function(grunt){
 	 });
 
 	// grunt.loadNpmTasks('grunt-contrib-cssmin');
-}
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+};
