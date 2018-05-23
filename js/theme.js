abfdf9b384c4fdeae461e8628674ee8f3680a39f
Pranta Saha <sahapranta@gmail.com>
2018-05-23 14:19:44 +0600

    00

--- js/theme.js
+++ js/theme.js
@@ -140,5 +140,12 @@ $(function($) {
       });
     }); 
 
+      // preloader js
+    $(window).load(function() { // makes sure the whole site is loaded
+        $('#preloader_spinner').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(100).css({'overflow':'visible'})
+    });
+
     
 });
